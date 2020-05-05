ae5d92f95eba1a758572ce93031ab6ea24e96e3c
Swarggg <swarg@inbox.ru>
2020-05-06 02:37:12 +0300

    Добавлен класс для работы с датами (Calendar)

---
+++ src/TestCalendar.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+public class TestCalendar {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(2020,0,1,21,0,0);
+        long day0 = cal.getTimeInMillis();
+        day0 +=1000*60*60; //+1 hour (+=...  same as  day0=day0+...)
+
+        cal.setTimeInMillis(day0); // update time
+        System.out.println("plus one hour: "+cal.get(cal.HOUR_OF_DAY));
+
+        cal.add(cal.DATE, 32);
+        System.out.println("add 32 days: "+cal.getTime());
+
+        cal.roll(cal.DATE, 32);
+        System.out.println("Roll time for 32 days: "+cal.getTime());
+
+        cal.set(cal.DATE, 1);
+        System.out.println("Set day in 1: "+cal.getTime());
+
+    }
+
+}
--- src/TestFormats.java
+++ src/TestFormats.java
@@ -1,6 +1,21 @@
+import java.util.Date;
+
 public class TestFormats {
     public static void main(String[] args) {
-        String s = String.format("Мне нужно округлить число %,.3f. Да, вот так.", 1100.456550);
+        double a = 1100.456550;
+        String s = String.format("Мне нужно округлить число "+a+" до %,.3f. Да, вот так.", a);
         System.out.println(s);
+
+        String d = String.format("%tc", new Date());
+        System.out.println(d);
+
+        String t = String.format("%tr", new Date());
+        System.out.println(t);
+
+        Date td = new Date();
+        String today = String.format("%tA, %td %tB %tY %<tH:%<tM", td, td, td, td); // знак < заставлет использовать предыдущий аргумент td
+        System.out.println(today);
+
+
     }
 }
